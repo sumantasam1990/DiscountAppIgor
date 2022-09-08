abc179b3e8877fb2d6d615d87cede432b371239f
Sumanta Kundu <sumantasam1990@gmail.com>
2022-09-08 08:30:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DiscountApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
