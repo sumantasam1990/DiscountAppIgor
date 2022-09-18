4dac6329debe8575b0df9770d6056ce08fe7f3e9
Sumanta Kundu <sumantasam1990@gmail.com>
2022-09-18 21:40:11 +0530

    18th sep 22

--- src/app/level-one/level-one.component.ts
+++ src/app/level-one/level-one.component.ts
@@ -10,11 +10,11 @@ import { environment } from 'src/environments/environment';
 })
 export class LevelOneComponent implements OnInit {
 
-  usersURL: string = environment.serverAPI + 'level/one/show/'
-  cateId: string
-  cateName: string
-  data: any = []
-  loading: boolean = false
+  usersURL: string = environment.serverAPI + 'level/one/show/';
+  cateId: string;
+  cateName: string;
+  data: any = [];
+  loading = false;
 
   constructor(
     private http: HttpClient,
@@ -23,23 +23,24 @@ export class LevelOneComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.cateId = this.route.snapshot.paramMap.get('id')
-    this.cateName = this.route.snapshot.paramMap.get('name')
-    this.getData()
+    this.cateId = this.route.snapshot.paramMap.get('id');
+    this.cateName = this.route.snapshot.paramMap.get('name');
+    this.getData();
   }
 
   getData() {
-    this.loading = true
+    this.loading = true;
       this.http.get(this.usersURL + this.cateId).subscribe((res: any) => {
-        this.loading = false
-        this.data = res.level_ones
+        this.loading = false;
+        this.data = res.level_ones;
       }, error => {
-        alert('Error! ' + error)
-      })
+        alert('Error! ' + error);
+      });
   }
 
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   level_two_screen(id: number, name: string) {
-    this.router.navigate(['level-two', id, name])
+    this.router.navigate(['level-two', id, name]);
   }
 
 }
